90926c6247336b37267ec0b44f3472647a9219c1
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-03-03 22:48:48 +0200

    Publishing the Slot Game

---
+++ slot.js
@@ -0,0 +1,161 @@
+// Code modified from template that has been taken from this repository  --> https://github.com/odhyan/slot 
+
+$(document).ready(function() {
+    /**
+    * Global variables
+    */
+    var completed = 0;
+    /**
+    * @class Slot
+    * @constructor
+    */
+    function Slot(el, max, step) {
+        this.speed = 0; //speed of the slot at any point of time
+        this.step = step; //speed will increase at this rate
+        this.si = null; //holds setInterval object for the given slot
+        this.el = el; //dom element of the slot
+        this.maxSpeed = max; //max speed this slot can have
+        this.pos = null; //final position of the slot    
+
+        $(el).pan({
+            fps:30,
+            dir:'down'
+        });
+        $(el).spStop();
+    }
+
+    /**
+    * @method start
+    * Starts a slot
+    */
+    Slot.prototype.start = function() {
+        var _this = this;
+        $(_this.el).addClass('motion');
+        $(_this.el).spStart();
+        _this.si = window.setInterval(function() {
+            if(_this.speed < _this.maxSpeed) {
+                _this.speed += _this.step;
+                $(_this.el).spSpeed(_this.speed);
+            }
+        }, 100);
+    };
+
+    /**
+    * @method stop
+    * Stops a slot
+    */
+    Slot.prototype.stop = function() {
+        var _this = this,
+            limit = 30;
+        clearInterval(_this.si);
+        _this.si = window.setInterval(function() {
+            if(_this.speed > limit) {
+                _this.speed -= _this.step;
+                $(_this.el).spSpeed(_this.speed);
+            }
+            if(_this.speed <= limit) {
+                _this.finalPos(_this.el);
+                $(_this.el).spSpeed(0);
+                $(_this.el).spStop();
+                clearInterval(_this.si);
+                $(_this.el).removeClass('motion');
+                _this.speed = 0;
+            }
+        }, 100);
+    };
+
+    /**
+    * @method finalPos
+    * Finds the final position of the slot
+    */
+    Slot.prototype.finalPos = function() {
+        var el_id = $(this.el).attr('id');
+        $._spritely.instances[el_id].t = 0;
+        $(this.el).css('background-position', '0px 4px');
+        this.speed = 0;
+        completed++;
+        $('#result').html('');
+    };
+    
+    /**
+    * @method reset
+    * Reset a slot to initial state
+    */
+    Slot.prototype.reset = function() {
+        var el_id = $(this.el).attr('id');
+        $._spritely.instances[el_id].t = 0;
+        $(this.el).css('background-position', '0px 4px');
+        this.speed = 0;
+        completed = 0;
+        $('#result').html('');
+    };
+
+    function enableControl() {
+        $('#control').attr("disabled", false);
+    }
+
+    function disableControl() {
+        $('#control').attr("disabled", true);
+    }
+
+    function printResult() {
+        var res;
+        if(1 == 1) {
+            res = "Iso voitto! Voitte lunastaa voittonne ottamalla yhteyttä aapo.soukkio@edu.savonia.fi tai puhelimitse 0452334618 ";
+        } else {
+            res = "You Lose";
+        }
+        $('#result').html(res);
+    }
+
+    //create slot objects
+    var a = new Slot('#slot1', 30, 1),
+        b = new Slot('#slot2', 45, 2),
+        c = new Slot('#slot3', 70, 3);
+
+    /**
+    * Slot machine controller
+    */
+    $('#control').click(function() {
+        var x;
+        if(this.innerHTML == "Start") {
+            a.start();
+            b.start();
+            c.start();
+            this.innerHTML = "Stop";
+            
+            disableControl(); //disable control until the slots reach max speed
+            
+            //check every 100ms if slots have reached max speed 
+            //if so, enable the control
+            x = window.setInterval(function() {
+                if(a.speed >= a.maxSpeed && b.speed >= b.maxSpeed && c.speed >= c.maxSpeed) {
+                    enableControl();
+                    window.clearInterval(x);
+                }
+            }, 100);
+        } else if(this.innerHTML == "Stop") {
+            a.stop();
+            b.stop();
+            c.stop();
+            this.innerHTML = "Reset";
+
+            disableControl(); //disable control until the slots stop
+            
+            //check every 100ms if slots have stopped
+            //if so, enable the control
+            x = window.setInterval(function() {
+                if(a.speed === 0 && b.speed === 0 && c.speed === 0 && completed === 3) {
+                    enableControl();
+                    window.clearInterval(x);
+                    printResult();
+                }
+            }, 100);
+        } else { //reset
+            a.reset();
+            b.reset();
+            c.reset();
+            this.innerHTML = "Start";
+        }
+    });
+});
